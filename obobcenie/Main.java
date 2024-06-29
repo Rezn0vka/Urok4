cd83c0612a6088fe3712adccce37c4803b08647d
Rezn0vka <160842293+Rezn0vka@users.noreply.github.com>
2024-06-29 11:37:44 +0600

    Create Main.java

---
+++ obobcenie/Main.java
@@ -0,0 +1,19 @@
+package obobcenie;
+
+import obobcenie.controller.TeacherController;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TeacherController teacherController = new TeacherController();
+        teacherController.create("Чалышев", "Денис", "Антонович");
+        teacherController.create("Химушин", "Радик", "Александрович");
+        teacherController.create("Егудин", "Александр", "Егорович");
+        teacherController.create("Кузяев", "Владимир", "Георгиевич");
+        teacherController.printTeachers();
+        System.out.println("Меняем учителя с TeacherID = 2");
+        teacherController.editTeacher(2, "Стриханов", "Ян", "Ильдарович");
+        teacherController.printTeachers();
+
+    }
+}
